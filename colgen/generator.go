b54aca16c9f2bea52e5926fcb572cac5fb54d6a9
Weimin Kong <im.wmkong@gmail.com>
2026-04-27 11:57:15 +0800

    Simplify error message for package validation

--- colgen/generator.go
+++ colgen/generator.go
@@ -111,7 +111,7 @@ func (g *Generator) checkSamePackage(models []any) error {
 	for _, model := range models[1:] {
 		currentPkg := reflectx.PackageName(model)
 		if currentPkg != pkgName {
-			return fmt.Errorf("all models must be in the same package or explicitly set WithPackageName")
+			return fmt.Errorf("all models must be in the same package")
 		}
 	}
 	return nil
